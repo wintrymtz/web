a4c0aa390d2cdbc48d0e6e8c185620b330ed5319
GuillermoMorin <111553871+GuillermoMorin@users.noreply.github.com>
2025-04-09 02:56:38 -0600

    Agregando redireccionamiento de Home

--- front/src/MovieCard.jsx
+++ front/src/MovieCard.jsx
@@ -1,13 +1,33 @@
 import "./css/Home.css"; // Importamos el CSS
+import { useNavigate } from "react-router-dom";
+
+function StarRating({ rating }) {
+    const stars = [];
+    const filledStars = Math.floor(rating);
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <span key={i} style={{ color: i <= filledStars ? "#ffd700" : "#ccc" }}>★</span>
+      );
+    }
+    return <div className="star-ratingHome">{stars}</div>;
+}
 
 function MovieCard({ title, image, description, calif, year }) {
 
-    return (<div className="movie-card">
+    const navigate = useNavigate();
+    const numericRating = parseFloat(calif.split("/")[0]) / 2;
+
+    const handleClick = () => {
+      navigate("/details");
+    };
+
+    return (
+        <div className="movie-card" onClick={handleClick}>
         <img src={image} alt={title} />
         <h3>{title}</h3>
+        <StarRating rating={numericRating} />
         <p>{description}</p>
-        <p>{`calif: ${calif}`}</p>
-        <p>{`year: ${year}`}</p>
+        <p>{`Año: ${year}`}</p>
     </div>);
 }
 
